0c51d0a68cfccb6efbfa8a15411b80ea48d83414
ganeshkarbharigvk <114089904+ganeshkarbharigvk@users.noreply.github.com>
2022-09-22 11:07:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,163 +1,46 @@
+import pandas as pd
 import streamlit as st
-import numpy as np
-from pandas import DataFrame
 from keybert import KeyBERT
+import yake
 from keyphrase_vectorizers import KeyphraseCountVectorizer
-import seaborn as sns
-# For download buttons
-from functionforDownloadButtons import download_button
-import os
-import json
 
-st.set_page_config(
-    page_title="Patent Keyword Extractor",
-    page_icon="🎈",
-)
+@st.cache(allow_output_mutation=True, suppress_st_warning=True, show_spinner=True)
+def load_model():
+  model = KeyBERT("AI-Growth-Lab/PatentSBERTa")
+  return model
+  
+model = load_model()
+st.title("Patent Text Extractor")
+placeholder = st.empty()
+text_input = placeholder.text_area("Paste or write text", height=300)
+button = st.button("Extract Keywords")
+top_n = st.sidebar.slider("Select a number of keywords", 1, 10, 50,20)
+min_ngram = st.sidebar.number_input("Minimum number of words in each keyword", 1, 5, 1, 1)
+max_ngram = st.sidebar.number_input("Maximum number of words in each keyword", min_ngram, 5, 3, step=1)
+st.sidebar.code(f"ngram_range=({min_ngram}, {max_ngram})")
 
+params = {"docs": text_input, "top_n": top_n, "keyphrase_ngram_range": (min_ngram, max_ngram), "stop_words": 'english',"vectorizer":KeyphraseCountVectorizer()}
 
-def _max_width_():
-    max_width_str = f"max-width: 1400px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True,
-    )
+add_diversity = st.sidebar.checkbox("Adjust diversity of keywords")
 
+if add_diversity:
+  method = st.sidebar.selectbox("Select a method", ("Max Sum Similarity", "Maximal Marginal Relevance"))
+  if method == "Max Sum Similarity":
+        nr_candidates = st.sidebar.slider("nr_candidates", 20, 50, 20, 2)
+        params["use_maxsum"] = True
+        params["nr_candidates"] = nr_candidates
 
-_max_width_()
+  elif method == "Maximal Marginal Relevance":
+        diversity = st.sidebar.slider("diversity", 0.1, 1.0, 0.6, 0.01)
+        params["use_mmr"] = True
+        params["diversity"] = diversity
 
-c30, c31, c32 = st.columns([2.5, 1, 3])
+keywords = model.extract_keywords(**params)
 
-with c30:
-    # st.image("logo.png", width=400)
-    st.title("🔑 Patent Keyword Extractor")
-    st.header("")
-
-
-
-with st.expander("ℹ️ - About this app", expanded=True):
-
-    st.write(
-        """     
--   The *Patent Keyword Extractor* is using KeyBERT with PatentsBERT   """
-    )
-
-    st.markdown("")
-
-st.markdown("")
-st.markdown("## **📌 Paste document **")
-with st.form(key="my_form"):
-
-
-    ce, c2, c3 = st.columns([0.07, 5, 0.07])
-    with c1:
-
-	def load_model():
-	      model = KeyBERT("AI-Growth-Lab/PatentSBERTa")
-	      return model
-
-        top_N = st.slider(
-            "# of results",
-            min_value=5,
-            max_value=50,
-            value=50,
-            help="You can choose the number of keywords/keyphrases to display. Between 5 and 50, default number is 50.",
-        )
-        min_Ngrams = st.number_input(
-            "Minimum Ngram",
-            min_value=1,
-            max_value=3,
-            help="""The minimum value for the ngram range.
-*Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases.
-To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 2) or higher depending on the number of words you would like in the resulting keyphrases.""",
-            # help="Minimum value for the keyphrase_ngram_range. keyphrase_ngram_range sets the length of the resulting keywords/keyphrases. To extract keyphrases, simply set keyphrase_ngram_range to (1, # 2) or higher depending on the number of words you would like in the resulting keyphrases.",
-        )
-
-        max_Ngrams = st.number_input(
-            "Maximum Ngram",
-            value=3,
-            min_value=1,
-            max_value=3,
-            help="""The maximum value for the keyphrase_ngram_range.
-*Keyphrase_ngram_range* sets the length of the resulting keywords/keyphrases.
-To extract keyphrases, simply set *keyphrase_ngram_range* to (1, 3) or higher depending on the number of words you would like in the resulting keyphrases.""",
-        )
-
-        StopWordsCheckbox = st.checkbox(
-            "Remove stop words",
-            help="Tick this box to remove stop words from the document (currently English only)",
-        )
-
-        use_MMR = st.checkbox(
-            "Use MMR",
-            value=True,
-            help="You can use Maximal Margin Relevance (MMR) to diversify the results. It creates keywords/keyphrases based on cosine similarity. Try high/low 'Diversity' settings below for interesting variations.",
-        )
-
-        Diversity = st.slider(
-            "Keyword diversity (MMR only)",
-            value=0.5,
-            min_value=0.0,
-            max_value=1.0,
-            step=0.1,
-            help="""The higher the setting, the more diverse the keywords.
-            
-Note that the *Keyword diversity* slider only works if the *MMR* checkbox is ticked.
-""",
-        )
-
-    with c2:
-        doc = st.text_area(
-            "Paste your text below (max 4000 words)",
-            height=610,
-        )
-
-        MAX_WORDS = 4000
-        import re
-        res = len(re.findall(r"\w+", doc))
-        if res > MAX_WORDS:
-            st.warning(
-                "⚠️ Your text contains "
-                + str(res)
-                + " words."
-                + " Only the first 4000 words will be reviewed. Stay tuned as increased allowance is coming! 😊"
-            )
-
-            doc = doc[:MAX_WORDS]
-
-        submit_button = st.form_submit_button(label="✨ Extract Keywords!")
-
-    if use_MMR:
-        mmr = True
-    else:
-        mmr = False
-
-    if StopWordsCheckbox:
-        StopWords = "english"
-    else:
-        StopWords = None
-
-if not submit_button:
-    st.stop()
-
-if min_Ngrams > max_Ngrams:
-    st.warning("min_Ngrams can't be greater than max_Ngrams")
-    st.stop()
-
-keywords = kw_model.extract_keywords(
-    doc,
-    keyphrase_ngram_range=(min_Ngrams, max_Ngrams),
-    #use_mmr=mmr,
-    stop_words=StopWords,
-    vectorizer=KeyphraseCountVectorizer(),
-    top_n=top_N,
-    #diversity=Diversity,
-)
+if keywords != []:
+    st.info("Extracted keywords")
+    keywords = pd.DataFrame(keywords, columns=["Keyword", "Score"])
+    st.table(keywords)
 
 st.markdown("## **🎈 Check & download results **")
 
@@ -175,8 +58,8 @@ with c3:
 st.header("")
 
 df = (
-    DataFrame(keywords, columns=["Keyword/Keyphrase", "Relevancy"])
-    .sort_values(by="Relevancy", ascending=False)
+    DataFrame(keywords, columns=["Keyword/Keyphrase", "Score"])
+    .sort_values(by="Score", ascending=False)
     .reset_index(drop=True)
 )
 
